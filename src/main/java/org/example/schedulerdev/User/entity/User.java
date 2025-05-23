4eeadaf222745d2749abcdc36e06c82b40f3ad0d
KimDohwan24 <94787456+KimDohwan24@users.noreply.github.com>
2025-05-23 11:37:11 +0900

    add Validation

--- src/main/java/org/example/schedulerdev/User/entity/User.java
+++ src/main/java/org/example/schedulerdev/User/entity/User.java
@@ -1,6 +1,8 @@
 package org.example.schedulerdev.User.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.schedulerdev.common.BaseEntity;
@@ -15,13 +17,14 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @NotEmpty
     private String username;
 
-    @Column(nullable = false)
+    @NotEmpty
     private String password;
 
-    @Column(nullable = false)
+    @NotEmpty
+    @Email
     private String useremail;
 
     public User(String username, String password, String useremail) {
